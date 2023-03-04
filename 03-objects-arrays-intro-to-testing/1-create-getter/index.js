5d38eb66e36232bb76ff9be7fd529a0d50691349
Stepkin Alexander <stepkin@speechpro.com>
2023-03-04 19:01:43 +0300

    createGetter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,28 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+	
+  const tmpArray = path.split('.');
+  
+  return function(obj) {
+    
+    if (!obj) {
+      console.error("There is no object");
+      return;
+    }  
+    // just for training ang safety
+    let copyObj = {...obj};
+        
+    for (let i = 0; i < tmpArray.length; i++) {
+      
+      if (!copyObj[tmpArray[i]]) {
+        console.error(`The key ${copyObj[tmpArray[i]]} not found! Please check the path`);
+        return;
+      }
+      
+      copyObj = copyObj[tmpArray[i]];
+    }
+    return copyObj;
+  };
 
 }
