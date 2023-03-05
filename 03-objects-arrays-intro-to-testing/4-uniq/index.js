c37a252442ae4b1ae108290223d0b47c919979f0
Stepkin Alexander <stepkin@speechpro.com>
2023-03-05 21:07:34 +0300

    uniq fn added

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,16 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
-
+  const state = [];
+  const set = new Set(arr);
+  set.forEach(value => state.push(value));
+  return state;
 }
+
+
+console.log(uniq(['a', 'a', 'b', 'c', 'c'])); // [1, 2, 3, 4]
+
+/*
+uniq([1, 2, 2, 3, 1, 4]); // [1, 2, 3, 4]
+uniq(['a', 'a', 'b', 'c', 'c']); // ['a', 'b', 'c']
+*/
